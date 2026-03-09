b79edf151e7d5c7730f47e5bb2d5507476205c4a
dgrasman <dgrasman@usfca.edu>
2026-03-08 18:22:22 -0700

    Set up py

---
+++ gini.py
@@ -0,0 +1,7 @@
+import wbgapi as wb
+import pandas as pd
+import geopandas as gdp
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# World Bank Indicators: https://data.worldbank.org/indicator/SI.POV.GINI
